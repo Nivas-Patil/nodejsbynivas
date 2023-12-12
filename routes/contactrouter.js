47fcb24fec8ec1db9b013674bdd506262769c46d
Nivas-Patil <nivaspatil0750@gmail.com>
2023-12-12 10:36:37 +0530

    updated comment of routes

--- routes/contactrouter.js
+++ routes/contactrouter.js
@@ -1,12 +1,12 @@
 const Router = require('router')
 const router = Router();
-
+//set cookie
 router.get('/',(req,res)=>{
     res.cookie('setName','Nivas-Patil-Kolhapurkar');
     res.send('cookie set successfully');
 })
 
-// set cookie
+// delete cookie
 router.get('/clear',(req,res)=>{
     res.clearCookie('setName');
     res.send('cookie deleted');
